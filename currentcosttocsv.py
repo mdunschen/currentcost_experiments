5c7d57a4d0c6234333ab41b8998ed81eb68f5fd2
Martin Dunschen <mdunschen@gmail.com>
2014-11-29 18:56:29 +0000

    added sun altitude

--- currentcosttocsv.py
+++ currentcosttocsv.py
@@ -1,11 +1,36 @@
 import sys, os
 import re
-import time
+import time, datetime
+import Pysolar
+
+lat, lon = 53.373242,-2.86108
+
+def Getdatetime(t):
+    return datetime.datetime.utcfromtimestamp(t)
+
+def GetAsSeconds(tnow):
+    t0 = time.mktime(time.strptime(tnow, "%Y %m %d %H:%M:%S"))
+    return t0
+    
+
+def GetSunAltitude(dateofdump, hrange):
+    t0 = GetAsSeconds(dateofdump)
+    h0 = (hrange - 0) * 3600
+    h1 = (hrange - 1) * 3600
+    dh0 = (t0 - h0)
+    dh1 = (t0 - h1)
+    dt0 = Getdatetime(dh0)
+    dt1 = Getdatetime(dh1)
+
+    alt0 = Pysolar.GetAltitude(lat, lon, dt0)
+    alt1 = Pysolar.GetAltitude(lat, lon, dt1)
+    return (alt0, alt1)
+
 
 def ConvertToClock(tnow, hrange):
     # h="hours", 24="22 to 24 hrs ago"
     # tnow in seconds since the millenium
-    t0 = time.mktime(time.strptime(tnow, "%Y %m %d %H:%M:%S"))
+    t0 = GetAsSeconds(tnow)
     #print tnow, t0, hrange
     h0 = (hrange - 0) * 3600 # seconds
     h1 = (hrange - 2) * 3600 # seconds
@@ -54,11 +79,12 @@ def ConvertToCSV(cc, fn, fdate):
             hrange = int(hh[1:])
             clockrange = ConvertToClock(dateofdump, hrange)
             val = float(dset[hh])
-            values.append((clockrange, val))
+            alt0, alt1 = GetSunAltitude(dateofdump, hrange)
+            values.append((clockrange, val, alt0, alt1))
     f = open(fn, "w")
-    f.write("%s, %s\n" % ("Date", "Energy [%s]" % u))
+    f.write("%s, %s, %s, %s\n" % ("Date", "Energy [%s]" % u, "Sun Altitude From",  "Sun Altitude To"))
     for rv in values:
-        f.write("%s, %f\n" % rv)
+        f.write("%s, %f, %f, %f\n" % rv)
     f.close()
     
 
