e55f060d0ff7cc112aa84080aef6eae025bd07a4
Martin Dunschen <mdunschen@gmail.com>
2014-12-08 21:51:12 +0000

    changed timestamp format

--- currentcosttocsv.py
+++ currentcosttocsv.py
@@ -8,16 +8,13 @@ lat, lon = 53.373242,-2.86108
 def Getdatetime(t):
     return datetime.datetime.utcfromtimestamp(t)
 
-def GetAsSeconds(tnow, bFullHours):
-    if bFullHours:
-        t0 = time.mktime(time.strptime(tnow, "%Y %m %d %H:00:00"))
-    else:
-        t0 = time.mktime(time.strptime(tnow, "%Y %m %d %H:%M:%S"))
+def GetAsSeconds(tnow):
+    t0 = time.mktime(time.strptime(tnow, "%Y %m %d %H:%M:%S"))
     return t0
     
 
 def GetSunAltitude(dateofdump, hrange):
-    t0 = GetAsSeconds(dateofdump, True)
+    t0 = GetAsSeconds(dateofdump)
     h0 = (hrange - 0) * 3600
     h1 = (hrange - 1) * 3600
     dh0 = (t0 - h0)
@@ -33,18 +30,22 @@ def GetSunAltitude(dateofdump, hrange):
 def ConvertToClock(tnow, hrange):
     # h="hours", 24="22 to 24 hrs ago"
     # tnow in seconds since the millenium
-    t0 = GetAsSeconds(tnow, True)
+    t0 = GetAsSeconds(tnow)
     #print tnow, t0, hrange
     h0 = (hrange - 0) * 3600 # seconds
     h1 = (hrange - 2) * 3600 # seconds
     # same day?
     lt0 = time.localtime(t0 - h0)
     lt1 = time.localtime(t0 - h1)
-    if time.strftime("%Y/%m/%d", lt0) == time.strftime("%Y/%m/%d", lt1):
-        return "%s - %s" % (time.strftime("%Y/%m/%d %H:%M:%S", lt0), time.strftime("%H:%M:%S", lt1))
-    else:
-        return "%s - %s" % (time.strftime("%Y/%m/%d %H:%M:%S", lt0), time.strftime("%Y/%m/%d %H:%M:%S", lt1))
+    return (lt0, lt1)
 
+def FormatTimeRange(timerange, bAddDay):
+    lt0, lt1 = timerange
+    sameday = time.strftime("%Y/%m/%d", lt0) == time.strftime("%Y/%m/%d", lt1)
+    if bAddDay:
+        return (sameday, "%s %s-%s" % (time.strftime("%Y/%m/%d", lt0), time.strftime("%H", lt0), time.strftime("%Hh", lt1)))
+    else:
+        return (sameday, "%s-%s" % (time.strftime("%H", lt0), time.strftime("%Hh", lt1)))
 
 def addCCToValues(cc, values, fdate):
     # history
@@ -60,7 +61,6 @@ def addCCToValues(cc, values, fdate):
     u = h['units']
     #expecting data entries
     datakeys = [k for k in h.keys() if re.match('data*', k)]
-    print datakeys
 
     # should be 9 sets of data
     assert len(datakeys) == 10
@@ -90,9 +90,7 @@ def addCCToValues(cc, values, fdate):
                 alt0 = alt0 / altmax
                 alt1 = alt1 / altmax
             if clockrange in values:
-                # does this happen?
-                print values[clockrange]
-                print (val, alt0, alt1)
+                assert values[clockrange] == (val, alt0, alt1)
             else:
                 values[clockrange] = (val, alt0, alt1)
     
@@ -106,8 +104,11 @@ def ConvertToCSV(historylogs, fn, fdate):
     f = open(fn, "w")
     f.write("%s, %s, %s, %s\n" % ("Date", "Energy [%s]" % 'kWh', "Sun Altitude From",  "Sun Altitude To"))
     skeys = sorted(values.keys())
+    sd = True
     for sk in skeys:
-        f.write("%s, %f, %f, %f\n" % ((sk,) + values[sk]))
+        lsd, ts = FormatTimeRange(sk, sd)
+        f.write("%s, %f, %f, %f\n" % ((ts,) + values[sk]))
+        sd = not lsd
     f.close()
 
 
